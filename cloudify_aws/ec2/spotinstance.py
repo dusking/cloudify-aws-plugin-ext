4be1b4ddee04333fc9403f11f092bfe2b219fdb1
Omer Duskin <omerd@gigaspaces.com>
2016-11-19 17:45:41 +0200

    wip

--- cloudify_aws/ec2/spotinstance.py
+++ cloudify_aws/ec2/spotinstance.py
@@ -176,16 +176,16 @@ class SpotInstance(Instance):
         res = self.execute(self.client.cancel_spot_instance_requests,
                            dict(request_ids=[spot_req_id]),
                            raise_on_falsy=True)
-        logger.info('Requests terminated: {0}'.format(res))
+        ctx.logger.info('Requests terminated: {0}'.format(res))
 
     def _create_spot_instances(self, args=None, **kwargs):
         job_instance_id = None
 
         for price in self._pricing_history:
-            logger.info('Creating instance with price: {0}'.format(price))
+            ctx.logger.info('Creating instance with price: {0}'.format(price))
             job_instance_id = self._create_spot_instances_at_price(args, price=price, **kwargs)
             if job_instance_id:
                 break
-            logger.warning('Creating instance with price: {0} Failed'.format(price))
+            ctx.logger.warning('Creating instance with price: {0} Failed'.format(price))
 
         return job_instance_id
